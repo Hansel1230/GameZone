32c5bab750abaa312eb9ead8a64f39cb58c3b233
hansel1230 <hanseldelossantos1230@gmail.com>
2022-07-14 23:09:19 -0400

    Agregar archivos de proyecto.

---
+++ WindowsFormsAppGameZone/Interface/Login.Designer.cs
@@ -0,0 +1,271 @@
+﻿namespace WindowsFormsAppGameZone
+{
+    partial class FormLogin
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormLogin));
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.buttonCerrar = new System.Windows.Forms.Button();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBoxUsuario = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.labelUsuario = new System.Windows.Forms.Label();
+            this.labelContrasena = new System.Windows.Forms.Label();
+            this.textBoxContrasena = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel4 = new System.Windows.Forms.TableLayoutPanel();
+            this.buttonEntrar = new System.Windows.Forms.Button();
+            this.checkBoxMostrar = new System.Windows.Forms.CheckBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.tableLayoutPanel4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 2.031603F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 95.9368F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 1.805869F));
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel3, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel4, 1, 3);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 7.181329F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 42.7289F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 36.44524F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.00359F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(443, 557);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.buttonCerrar);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(12, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(419, 33);
+            this.panel1.TabIndex = 0;
+            // 
+            // buttonCerrar
+            // 
+            this.buttonCerrar.Dock = System.Windows.Forms.DockStyle.Right;
+            this.buttonCerrar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonCerrar.Location = new System.Drawing.Point(334, 0);
+            this.buttonCerrar.Name = "buttonCerrar";
+            this.buttonCerrar.Size = new System.Drawing.Size(85, 33);
+            this.buttonCerrar.TabIndex = 0;
+            this.buttonCerrar.Text = "Cerrar";
+            this.buttonCerrar.UseVisualStyleBackColor = true;
+            this.buttonCerrar.Click += new System.EventHandler(this.buttonCerrar_Click);
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.BackColor = System.Drawing.Color.Transparent;
+            this.tableLayoutPanel2.ColumnCount = 3;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 60F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel2.Controls.Add(this.pictureBox1, 1, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(12, 42);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(419, 231);
+            this.tableLayoutPanel2.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(86, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(245, 225);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBoxUsuario
+            // 
+            this.textBoxUsuario.Dock = System.Windows.Forms.DockStyle.Top;
+            this.textBoxUsuario.Location = new System.Drawing.Point(86, 39);
+            this.textBoxUsuario.Name = "textBoxUsuario";
+            this.textBoxUsuario.Size = new System.Drawing.Size(246, 20);
+            this.textBoxUsuario.TabIndex = 3;
+            this.textBoxUsuario.Text = "Ingrese su usuario";
+            this.textBoxUsuario.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBoxUsuario.Enter += new System.EventHandler(this.textBoxUsuario_Enter);
+            this.textBoxUsuario.Leave += new System.EventHandler(this.textBoxUsuario_Leave);
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 3;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20.04773F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 60.38186F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 19.80907F));
+            this.tableLayoutPanel3.Controls.Add(this.textBoxUsuario, 1, 1);
+            this.tableLayoutPanel3.Controls.Add(this.labelUsuario, 1, 0);
+            this.tableLayoutPanel3.Controls.Add(this.labelContrasena, 1, 2);
+            this.tableLayoutPanel3.Controls.Add(this.textBoxContrasena, 1, 3);
+            this.tableLayoutPanel3.Controls.Add(this.checkBoxMostrar, 2, 3);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(12, 279);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 4;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 18.36735F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 30.61225F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 19.89796F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 31.12245F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(419, 196);
+            this.tableLayoutPanel3.TabIndex = 4;
+            // 
+            // labelUsuario
+            // 
+            this.labelUsuario.AutoSize = true;
+            this.labelUsuario.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelUsuario.Location = new System.Drawing.Point(86, 0);
+            this.labelUsuario.Name = "labelUsuario";
+            this.labelUsuario.Size = new System.Drawing.Size(246, 36);
+            this.labelUsuario.TabIndex = 4;
+            this.labelUsuario.Text = "Usuario";
+            this.labelUsuario.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelContrasena
+            // 
+            this.labelContrasena.AutoSize = true;
+            this.labelContrasena.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelContrasena.Location = new System.Drawing.Point(86, 96);
+            this.labelContrasena.Name = "labelContrasena";
+            this.labelContrasena.Size = new System.Drawing.Size(246, 39);
+            this.labelContrasena.TabIndex = 5;
+            this.labelContrasena.Text = "Contraseña";
+            this.labelContrasena.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBoxContrasena
+            // 
+            this.textBoxContrasena.Dock = System.Windows.Forms.DockStyle.Top;
+            this.textBoxContrasena.Location = new System.Drawing.Point(86, 138);
+            this.textBoxContrasena.Name = "textBoxContrasena";
+            this.textBoxContrasena.Size = new System.Drawing.Size(246, 20);
+            this.textBoxContrasena.TabIndex = 6;
+            this.textBoxContrasena.Text = "Ingrese su contraseña";
+            this.textBoxContrasena.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBoxContrasena.Enter += new System.EventHandler(this.textBoxContrasena_Enter);
+            this.textBoxContrasena.Leave += new System.EventHandler(this.textBoxContrasena_Leave);
+            // 
+            // tableLayoutPanel4
+            // 
+            this.tableLayoutPanel4.ColumnCount = 3;
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 19.80907F));
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 60.38186F));
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 19.80907F));
+            this.tableLayoutPanel4.Controls.Add(this.buttonEntrar, 1, 0);
+            this.tableLayoutPanel4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel4.Location = new System.Drawing.Point(12, 481);
+            this.tableLayoutPanel4.Name = "tableLayoutPanel4";
+            this.tableLayoutPanel4.RowCount = 2;
+            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 58.90411F));
+            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 41.09589F));
+            this.tableLayoutPanel4.Size = new System.Drawing.Size(419, 73);
+            this.tableLayoutPanel4.TabIndex = 5;
+            // 
+            // buttonEntrar
+            // 
+            this.buttonEntrar.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonEntrar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonEntrar.Location = new System.Drawing.Point(86, 3);
+            this.buttonEntrar.Name = "buttonEntrar";
+            this.buttonEntrar.Size = new System.Drawing.Size(247, 37);
+            this.buttonEntrar.TabIndex = 0;
+            this.buttonEntrar.Text = "Entrar";
+            this.buttonEntrar.UseVisualStyleBackColor = true;
+            this.buttonEntrar.Click += new System.EventHandler(this.buttonEntrar_Click);
+            // 
+            // checkBoxMostrar
+            // 
+            this.checkBoxMostrar.AutoSize = true;
+            this.checkBoxMostrar.Checked = true;
+            this.checkBoxMostrar.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBoxMostrar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.checkBoxMostrar.Location = new System.Drawing.Point(338, 138);
+            this.checkBoxMostrar.Name = "checkBoxMostrar";
+            this.checkBoxMostrar.Size = new System.Drawing.Size(78, 17);
+            this.checkBoxMostrar.TabIndex = 7;
+            this.checkBoxMostrar.Text = "Mostrar";
+            this.checkBoxMostrar.UseVisualStyleBackColor = true;
+            this.checkBoxMostrar.CheckedChanged += new System.EventHandler(this.checkBoxMostrar_CheckedChanged);
+            // 
+            // FormLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(443, 557);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FormLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.FormLogin_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.tableLayoutPanel3.PerformLayout();
+            this.tableLayoutPanel4.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button buttonCerrar;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.TextBox textBoxUsuario;
+        private System.Windows.Forms.Label labelUsuario;
+        private System.Windows.Forms.Label labelContrasena;
+        private System.Windows.Forms.TextBox textBoxContrasena;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel4;
+        private System.Windows.Forms.Button buttonEntrar;
+        private System.Windows.Forms.CheckBox checkBoxMostrar;
+    }
+}
+
---
+++ WindowsFormsAppGameZone/Interface/Login.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace WindowsFormsAppGameZone
+{
+    public partial class FormLogin : Form
+    {
+        public FormLogin()
+        {
+            InitializeComponent();
+        }
+
+        #region Eventos 
+        private void buttonEntrar_Click(object sender, EventArgs e)
+        {
+            string username, pass;
+            username = textBoxUsuario.Text;
+            pass = textBoxContrasena.Text;
+            MySqlConnection con = new MySqlConnection("server = 127.0.0.1; Database=gamezone; User Id = root; password=hansel1230");
+            try
+            {
+                con.Open();
+            }
+            catch (MySqlException ex) { MessageBox.Show("Error " + ex.ToString()); 
+            }
+
+            String sql = "select eNombre,eContrasena from empleado where eNombre ='" + username + "' AND eContrasena ='" + pass + "' ";
+            MySqlCommand comando = new MySqlCommand(sql, con);
+            MySqlDataReader read = comando.ExecuteReader();
+            if (read.Read())
+            {
+                this.Hide();
+                MessageBox.Show("Bienvenido " + username);
+
+            }
+            else if(textBoxUsuario.Text =="Ingrese su usuario" || textBoxContrasena.Text== "Ingrese su contraseña")
+            {
+                MessageBox.Show("Ingrese sus credenciales");
+            }
+            else
+            {
+                MessageBox.Show("No existe el usuario " + username);
+            }
+        }
+
+        private void buttonCerrar_Click(object sender, EventArgs e)
+        {
+            this.Close();   
+        }
+
+        private void textBoxUsuario_Leave(object sender, EventArgs e)
+        {
+            if (textBoxUsuario.Text == "")
+            {
+                textBoxUsuario.Text = "Ingrese su usuario";
+                textBoxUsuario.ForeColor = Color.Black;
+            }
+        }
+
+        private void textBoxUsuario_Enter(object sender, EventArgs e)
+        {
+            if (textBoxUsuario.Text == "Ingrese su usuario")
+            {
+                textBoxUsuario.Text = "";
+                textBoxUsuario.ForeColor = Color.Black;
+            }
+        }
+      
+        private void textBoxContrasena_Leave(object sender, EventArgs e)
+        {
+            if (textBoxContrasena.Text == "")
+            {
+                textBoxContrasena.Text = "Ingrese su contraseña";
+                textBoxContrasena.ForeColor = Color.Black;
+                textBoxContrasena.UseSystemPasswordChar = true;
+
+            }
+        }
+
+        private void textBoxContrasena_Enter(object sender, EventArgs e)
+        {
+            if (textBoxContrasena.Text == "Ingrese su contraseña")
+            {
+                textBoxContrasena.Text = "";
+                textBoxContrasena.ForeColor = Color.Black;
+                textBoxContrasena.UseSystemPasswordChar = false;
+            }
+        }
+        #endregion
+
+        private void checkBoxMostrar_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBoxMostrar.Checked == true)
+            {
+                textBoxContrasena.UseSystemPasswordChar = false;
+            }
+            else
+            {
+                textBoxContrasena.UseSystemPasswordChar = true;
+            }
+        }
+
+        private void FormLogin_Load(object sender, EventArgs e)
+        {
+             
+        }
+    }
+
+}
